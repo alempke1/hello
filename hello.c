7105faad6ddb5aee0dad6e1f7c5625c3fc4d6745
Austin Lempke <alempke2@huskers.unl.edu>
2020-08-19 21:34:29 -0500

    initial commit

---
+++ hello.c
@@ -0,0 +1,13 @@
+/** Austin Lempke and Eric Hinderer
+* 2020/09/19
+* A program that prints my name and my partners name in C
+*
+*/
+
+#include <stdlib.h>
+#include <stdio.h>
+
+int main(int argc, char **argv) {
+  printf("Austin Lempke\n", );
+  return 0;
+}
